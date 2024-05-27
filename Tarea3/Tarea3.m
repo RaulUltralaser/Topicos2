26d507e97f579455bf77efa86882d68f4ed91f5b
Raul <alex_lion345@hotmail.com>
2024-05-27 17:31:44 -0600

    Casi completo la tarea 3

--- Tarea3/Tarea3.m
+++ Tarea3/Tarea3.m
@@ -4,22 +4,33 @@ close all
 
 %Matriz de grado
 %Primer punto
-D=[0 0 0 0;
-   0 1 0 0;
-   0 0 1 0;
+% D=[0 0 0 0;
+%    0 1 0 0;
+%    0 0 1 0;
+%    0 0 0 1;];
+%Segundo punto
+D=[1 0 0 0;
+   0 2 0 0;
+   0 0 2 0;
    0 0 0 1;];
 
 
 %Matriz de adyacencia
 %Primer punto
-A=[0 0 0 0;
-   1 0 0 0;
-   0 1 0 0;
+% A=[0 0 0 0;
+%    1 0 0 0;
+%    0 1 0 0;
+%    0 0 1 0;];
+%Segundo punto
+A=[0 1 0 0;
+   1 0 1 0;
+   0 1 0 1;
    0 0 1 0;];
 
 
 %hago el objeto grafo, pero aún no lo ploteo
-G=digraph(A');
+% G=digraph(A');
+G=graph(A);
 
 %Nodos
 n=size(A,1);
@@ -54,14 +65,16 @@ Gamma_kron= kron(Gamma,Im);
 
 %% Esta parte es para la simulación del sistema
 %Valores iniciales
-% xi=[1;.9;.6;.3];
-xi=rand(size(Gamma_kron,1),1);
+xi0=[1;2;3;4;5;6;7;8;9;10;11;12];%Guardo los valores iniciales para referencia
+% xi0=rand(size(Gamma_kron,1),1);%Guardo los valores iniciales para referencia
+xi=xi0;%Uso estos valores en la aproximación de Euler
 
 %periodo de muestreo
 Dt=0.01;
-tiempo=20; %segundos
+tiempo=25; %segundos
 iteraciones=tiempo/Dt;
-% 
+
+%Simulo el sistema
 for k=1:iteraciones
 
     %Aproximación de Euler con Gamma_kron
@@ -70,9 +83,29 @@ for k=1:iteraciones
  
 end
 
+%Calculo de consensos
+syms t;
+Uno=ones(n,1);
+Cero=zeros(n,n);
+p=[1;0;0;0];
+egamma=[Uno*p' t*Uno*p' (1/2)*(t^2)*Uno*p';
+        Cero   Uno*p'   t*Uno*p';
+        Cero   Cero     Uno*p'];
+Consenso=egamma*xi0
+
 %Vectores de tiempo para gráficar
 t=linspace(0,tiempo,iteraciones+1);
 te=linspace(0,tiempo,iteraciones);
+
+figure
+subplot(1,2,1)
+plot(t,xi(1:4,:))
+grid on
+
+subplot(1,2,2)
+plot(t,xi(5:8,:))
+grid on
+
 
 figure
 plot(t,xi(9:12,:))
