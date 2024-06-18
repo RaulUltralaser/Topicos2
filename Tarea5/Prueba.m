36d491b5577c233264e5a6108b06e4c14a53cf90
RaulUltralaser <alex_lion345@hotmail.com>
2024-06-17 18:09:54 -0600

    agregue toda la tarea

--- Tarea5/Prueba.m
+++ Tarea5/Prueba.m
@@ -1,23 +1,9 @@
-clc
-clearvars 
-close all 
-
 % Parámetros de la curva de Lissajous
 A = 2;          % Amplitud en x
 B = 1;          % Amplitud en y
 a = 1;          % Frecuencia en x
 b = 2;          % Frecuencia en y
 delta = 0;      % Diferencia de fase
-
-AA=[0 0 1 0;
-    0 0 0 1;
-    0 0 0 0;
-    0 0 0 0];
-
-BB=[0 0;
-    0 0;
-    1 0;
-    0 1];
 
 % Tiempo
 t = linspace(0, 10, 1000); % Aumentar el rango de tiempo para la simulación
@@ -26,53 +12,64 @@ t = linspace(0, 10, 1000); % Aumentar el rango de tiempo para la simulación
 x_curve = A * sin(a * t + delta);
 y_curve = B * sin(b * t);
 
-vx_curve= A*a*cos(a*t);
-vy_curve= B*b*cos(b*t);
+% Derivadas
+dx_dt = A * a * cos(a * t + delta);          % Primera derivada de x(t)
+dy_dt = B * b * cos(b * t);                  % Primera derivada de y(t)
 
 % Distancia desde la curva para los agentes
 d = 1;
 
 % Posiciones relativas de los agentes con respecto a la curva
-x_offset = [d, 0, -d, 0];
-y_offset = [0, -d, 0, d];
+offsets = [d, 0; 0, d; -d, 0; 0, -d];
 
 % Parámetros de control
 k_a = 1;
-k_v = 5;
-k_p = 10;
+k_v = 2;
+k_p = 5;
 
-% Inicializar posiciones y velocidades de los agentes
-p = zeros(4, 1); 
+% Inicializar el vector de estados p para los agentes
+num_agents = 4;
+states = zeros(num_agents, 4, length(t)); % [posición_x, velocidad_x, posición_y, velocidad_y] para cada agente en cada tiempo
 
 % Inicializar estados
-%Primer punto
-p(:,1)=[x_curve(1) + x_offset(1);y_curve(1) + y_offset(1);0;0];
+for i = 1:num_agents
+    states(i, 1, 1) = x_curve(1) + offsets(i, 1); % Posiciones iniciales en x
+    states(i, 3, 1) = y_curve(1) + offsets(i, 2); % Posiciones iniciales en y
+end
 
-% Simulacion
+% Simulación
 dt = t(2) - t(1);
+AA = [0, 1, 0, 0; 0, 0, 0, 0; 0, 0, 0, 1; 0, 0, 0, 0];
+BB = [0, 0; 1, 0; 0, 0; 0, 1];
+
 for k = 1:length(t)-1
     % Posiciones deseadas en el siguiente instante de tiempo
-    p_des = [x_curve(k+1) + 1;y_curve(k+1)];
-    % Posicion actual
-    p_real= p(1:2,k);
-    %Error de posicion
-    e_p = p_des-p_real;
+    x_des = x_curve(k+1);
+    y_des = y_curve(k+1);
+    
+    for i = 1:num_agents
+        p_des = [x_des + offsets(i, 1); y_des + offsets(i, 2)];
+        v_des = [dx_dt(k+1); dy_dt(k+1)];
 
-    %Velocidad deseada
-    v_des=[vx_curve(k+1);vy_curve(k+1)];
-    %Velocidad actual
-    v_real=p(3:4,k);
-    %Error de velocidad
-    e_v = v_des-v_real;
-%     e_v=-k_p*e_p;
+        % Posicion actual
+        p_real = states(i, [1, 3], k);
+        v_real = states(i, [2, 4], k);
 
-    % Control de aceleracion
-    u = -k_a * (k_v * e_v - k_p * e_p);
+        % Error de posicion
+        e_p = p_des - p_real';
 
-    %Sistema
-    p(:,k+1)=p(:,k)-(AA*p(:,k)+BB*u)*dt;
+        % Error de velocidad
+        e_v = v_des - v_real';
 
-  
+        % Control de aceleracion
+        u = -k_a * (k_v * e_v - k_p * e_p);
+
+        % Actualizar estados
+        states(i, 2, k+1) = states(i, 2, k) + u(1) * dt; % velocidad_x
+        states(i, 4, k+1) = states(i, 4, k) + u(2) * dt; % velocidad_y
+        states(i, 1, k+1) = states(i, 1, k) + states(i, 2, k) * dt; % posición_x
+        states(i, 3, k+1) = states(i, 3, k) + states(i, 4, k) * dt; % posición_y
+    end
 end
 
 % Graficar la curva de Lissajous y las trayectorias de los agentes
@@ -85,19 +82,16 @@ plot(x_curve, y_curve, 'k', 'LineWidth', 1.5);
 % Colores para los agentes
 colors = ['r', 'g', 'b', 'm'];
 
-plot(p(1, :), p(2, :), 'Color', colors(1), 'LineWidth', 1.5);
-
+% Graficar las trayectorias de los agentes
+for i = 1:num_agents
+    plot(squeeze(states(i, 1, :)), squeeze(states(i, 3, :)), 'Color', colors(i), 'LineWidth', 1.5);
+    plot(states(i, 1, end), states(i, 3, end), 'o', 'Color', colors(i), 'MarkerSize', 10, 'LineWidth', 2); % Posición final
+end
 
 title('Curva de Lissajous con trayectorias de agentes controlados');
 xlabel('x(t)');
 ylabel('y(t)');
 grid on;
 axis equal;
+legend('Curva de Lissajous', 'Agente 1', 'Agente 2', 'Agente 3', 'Agente 4');
 hold off;
-
-
-% Graficar las trayectorias de los agentes
-% for i = 1:num_agents
-%     plot(squeeze(positions(i, 1, :)), squeeze(positions(i, 2, :)), 'Color', colors(i), 'LineWidth', 1.5);
-%     plot(positions(i, 1, 1), positions(i, 2, 1), 'o', 'Color', colors(i), 'MarkerSize', 10, 'LineWidth', 2); % Posición final
-% end
--- Tarea5/Tarea5.m
+++ Tarea5/Tarea5.m
@@ -9,75 +9,127 @@ a = 1;          % Frecuencia en x
 b = 2;          % Frecuencia en y
 delta = 0;      % Diferencia de fase
 
-AA=[0 1 0 0;
-    0 0 0 0;
+AA=[0 0 1 0;
     0 0 0 1;
+    0 0 0 0;
     0 0 0 0];
 
 BB=[0 0;
-    1 0;
     0 0;
+    1 0;
     0 1];
 
 % Tiempo
-t = linspace(0, 20, 1000); % Aumentar el rango de tiempo para la simulación
+tiempo=30; %tiempo en segundos
+t = linspace(0, tiempo, 1000); % Aumentar el rango de tiempo para la simulación
 
 % Ecuaciones paramétricas de la curva de Lissajous
 x_curve = A * sin(a * t + delta);
 y_curve = B * sin(b * t);
 
+vx_curve= A*a*cos(a*t);
+vy_curve= B*b*cos(b*t);
+
 % Distancia desde la curva para los agentes
-d = 1;
+d = .5;
 
 % Posiciones relativas de los agentes con respecto a la curva
-x_offset = [d, 0, -d, 0];
-y_offset = [0, -d, 0, d];
+%Primer punto
+% x_offset = [d, 0, -d, 0];
+% y_offset = [0, -d, 0, d];
+%Segundo punto
+x_offset = 5*[rand(), rand(), -rand(), rand()];
+y_offset = 10*[rand(), -rand(), rand(), rand()];
 
 % Parámetros de control
 k_a = 1;
-k_v = 10;
+k_v = 5;
 k_p = 10;
 
 % Inicializar posiciones y velocidades de los agentes
-num_agents = 4;
-positions = zeros(num_agents, 2, length(t)); % [x, y] para cada agente en cada tiempo
-velocities = zeros(num_agents, 2, length(t)); % [vx, vy] para cada agente en cada tiempo
+p = zeros(4, 1); 
+p2 = zeros(4, 1); 
+p3 = zeros(4, 1); 
+p4 = zeros(4, 1); 
 
 % Inicializar estados
 %Primer punto
-positions(:, 1, 1) = x_curve(1) + x_offset; % Posiciones iniciales en x
-positions(:, 2, 1) = y_curve(1) + y_offset; % Posiciones iniciales en y
-%Segundo punto
-% positions(:, 1, 1) = x_curve(1) + [rand(),rand(),rand(),rand()]; % Posiciones iniciales en x
-% positions(:, 2, 1) = y_curve(1) + [rand(),rand(),rand(),rand()]; %Posiciones iniciales en y
-% Simulación
+p(:,1)=[x_curve(1) + x_offset(1);y_curve(1) + y_offset(1);0;0];
+p2(:,1)=[x_curve(1) + x_offset(2);y_curve(1) + y_offset(2);0;0];
+p3(:,1)=[x_curve(1) + x_offset(3);y_curve(1) + y_offset(3);0;0];
+p4(:,1)=[x_curve(1) + x_offset(4);y_curve(1) + y_offset(4);0;0];
+
+% Simulacion
 dt = t(2) - t(1);
 for k = 1:length(t)-1
     % Posiciones deseadas en el siguiente instante de tiempo
-    x_des = x_curve(k+1) + x_offset;
-    y_des = y_curve(k+1) + y_offset;
+    p_des = [x_curve(k)+d;y_curve(k)];
+    p_des2 = [x_curve(k) ;y_curve(k)-d];
+    p_des3 = [x_curve(k) - d;y_curve(k)];
+    p_des4 = [x_curve(k) ;y_curve(k)+d];
+    % Posicion actual
+    p_real= p(1:2,k);
+    p_real2= p2(1:2,k);
+    p_real3= p3(1:2,k);
+    p_real4= p4(1:2,k);
+    %Error de posicion
+    e_p = p_des-p_real;
+    e_p2 = p_des2-p_real2;
+    e_p3 = p_des3-p_real3;
+    e_p4 = p_des4-p_real4;
 
-    for i = 1:num_agents
-        % Error de posición y velocidad
-        e_p = [x_des(i) - positions(i, 1, k); y_des(i) - positions(i, 2, k)];
-        e_v = -[velocities(i, 1, k); velocities(i, 2, k)];
+    %Velocidad deseada
+    v_des=[vx_curve(k+1);vy_curve(k+1)];
+    v_des2=[vx_curve(k+1);vy_curve(k+1)];
+    v_des3=[vx_curve(k+1);vy_curve(k+1)];
+    v_des4=[vx_curve(k+1);vy_curve(k+1)];
+    %Velocidad actual
+    v_real=p(3:4,k);
+    v_real2=p2(3:4,k);
+    v_real3=p3(3:4,k);
+    v_real4=p4(3:4,k);
+    %Error de velocidad
+    e_v = v_des-v_real;
+    e_v2 = v_des2-v_real2;
+    e_v3 = v_des3-v_real3;
+    e_v4 = v_des4-v_real4;
+    % e_v=-k_p*e_p;
 
-        % Control de aceleración
-        u = -k_a * (k_v * e_v + k_p * e_p);
+    % Control de aceleracion
+    u = -k_a * (k_v * e_v - k_p * e_p);
+    u2 = -k_a * (k_v * e_v2 - k_p * e_p2);
+    u3 = -k_a * (k_v * e_v3 - k_p * e_p3);
+    u4 = -k_a * (k_v * e_v4 - k_p * e_p4);
+    %Sistema
+    p(:,k+1)=p(:,k)-(AA*p(:,k)+BB*u)*dt;
+    p2(:,k+1)=p2(:,k)-(AA*p2(:,k)+BB*u2)*dt;
+    p3(:,k+1)=p3(:,k)-(AA*p3(:,k)+BB*u3)*dt;
+    p4(:,k+1)=p4(:,k)-(AA*p4(:,k)+BB*u4)*dt;
+    
+    % Control
+    ux(:,k)=u(1);
+    uy(:,k)=u(2);
 
-        % Actualizar posiciones
-        velocities(i, 1, k+1) = velocities(i, 1, k) - u(1) * dt;
-        velocities(i, 2, k+1) = velocities(i, 2, k) - u(2) * dt;
-        positions(i, 1, k+1) = positions(i, 1, k) + velocities(i, 1, k) * dt;
-        positions(i, 2, k+1) = positions(i, 2, k) + velocities(i, 2, k) * dt;
-        %Guardo estos datos para ploteos
-        epx(k)=e_p(1); %Error posición en x
-        epy(k)=e_p(2); %Error posición en y
+    %Errores en posiciones
+    epx(:,k)=e_p(1);
+    epy(:,k)=e_p(2);
+    epx2(:,k)=e_p2(1);
+    epy2(:,k)=e_p2(2);
+    epx3(:,k)=e_p3(1);
+    epy3(:,k)=e_p3(2);
+    epx4(:,k)=e_p4(1);
+    epy4(:,k)=e_p4(2);
 
-        evx(k)=e_v(1); %Error velocidad en x
-        evy(k)=e_v(2); %Error velocidad en y
-
-    end
+    %Errores en velocidad
+    evx(:,k)=e_v(1);
+    evy(:,k)=e_v(2);
+    evx2(:,k)=e_v2(1);
+    evy2(:,k)=e_v2(2);
+    evx3(:,k)=e_v3(1);
+    evy3(:,k)=e_v3(2);
+    evx4(:,k)=e_v4(1);
+    evy4(:,k)=e_v4(2);
+    
 end
 
 % Graficar la curva de Lissajous y las trayectorias de los agentes
@@ -90,46 +142,63 @@ plot(x_curve, y_curve, 'k', 'LineWidth', 1.5);
 % Colores para los agentes
 colors = ['r', 'g', 'b', 'm'];
 
-% Graficar las trayectorias de los agentes
-for i = 1:num_agents
-    plot(squeeze(positions(i, 1, :)), squeeze(positions(i, 2, :)), 'Color', colors(i), 'LineWidth', 1.5);
-    plot(positions(i, 1, 1), positions(i, 2, 1), 'o', 'Color', colors(i), 'MarkerSize', 10, 'LineWidth', 2); % Posición final
-end
+plot(p(1, :), p(2, :), 'Color', colors(1), 'LineWidth', 1.5);
+plot(p2(1, :), p2(2, :), 'Color', colors(2), 'LineWidth', 1.5);
+plot(p3(1, :), p3(2, :), 'Color', colors(3), 'LineWidth', 1.5);
+plot(p4(1, :), p4(2, :), 'Color', colors(4), 'LineWidth', 1.5);
+
+plot(p(1, 1), p(2, 1),'o', 'Color', colors(1), 'LineWidth', 1.5);
+plot(p2(1, 1), p2(2, 1),'o', 'Color', colors(2), 'LineWidth', 1.5);
+plot(p3(1, 1), p3(2, 1),'o', 'Color', colors(3), 'LineWidth', 1.5);
+plot(p4(1, 1), p4(2, 1),'o', 'Color', colors(4), 'LineWidth', 1.5);
 
 title('Curva de Lissajous con trayectorias de agentes controlados');
 xlabel('x(t)');
 ylabel('y(t)');
 grid on;
 axis equal;
-% legend('Curva de Lissajous', 'Agente 1', 'Agente 2', 'Agente 3', 'Agente 4');
 hold off;
 
-% figure
-% subplot(1,2,1)
-% plot(t(1,1:size(t,2)-1),epx);
-% title('Errores de posición en x')
-% ylabel('Error')
-% xlabel('tiempo s')
-% grid on
-% subplot(1,2,2)
-% plot(t(1,1:size(t,2)-1),epy);
-% title('Errores de posición en y')
-% ylabel('Error')
-% xlabel('tiempo s')
-% grid on
+figure
+subplot(2,1,1)
+plot(t(1,1:size(t,2)-1),epx);
+title('Errores de posición en x')
+ylabel('Error')
+xlabel('tiempo s')
+grid on
+subplot(2,1,2)
+plot(t(1,1:size(t,2)-1),epy);
+title('Errores de posición en y')
+ylabel('Error')
+xlabel('tiempo s')
+grid on
 % 
 % 
-% figure
-% subplot(1,2,1)
-% plot(t(1,1:size(t,2)-1),evx);
-% title('Errores de posición en x')
-% ylabel('Error')
-% xlabel('tiempo s')
-% grid on
-% subplot(1,2,2)
-% plot(t(1,1:size(t,2)-1),evy);
-% title('Errores de posición en y')
-% ylabel('Error')
-% xlabel('tiempo s')
-% grid on
+figure
+subplot(2,1,1)
+plot(t(1,1:size(t,2)-1),evx);
+title('Errores de velocidad en x')
+ylabel('Error')
+xlabel('tiempo s')
+grid on
+subplot(2,1,2)
+plot(t(1,1:size(t,2)-1),evy);
+title('Errores de velocidad en y')
+ylabel('Error')
+xlabel('tiempo s')
+grid on
 
+
+figure
+subplot(2,1,1)
+plot(t(1,1:size(t,2)-1),ux);
+title('Ley de control en x')
+ylabel('Control')
+xlabel('tiempo s')
+grid on
+subplot(2,1,2)
+plot(t(1,1:size(t,2)-1),uy);
+title('Ley de control en y')
+ylabel('Control')
+xlabel('tiempo s')
+grid on
