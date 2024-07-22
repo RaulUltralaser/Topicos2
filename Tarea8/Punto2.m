89a4cfedd748ebb7c8562d3996a45c0b1b1c7456
Raul <alex_lion345@hotmail.com>
2024-07-22 17:30:15 -0600

    punto dos de rogelio

--- Tarea8/Punto2.m
+++ Tarea8/Punto2.m
@@ -3,15 +3,15 @@ clearvars;
 close all;
 
 % Parámetros iniciales  tienen que ser mayores a cero
-l = 0.5; % Distancia del centro del robot al punto de control
+l = 1; % Distancia del centro del robot al punto de control
 k_x = 1; % Ganancia del controlador para error en x
 k_y = 1; % Ganancia del controlador para error en y
 k_theta = 1; % Ganancia del controlador para error en theta 
-kappa = 1; % Constante para el termino feed-forward 
+kappa = 3; % Constante para el termino feed-forward 
 n=10;       %Duración del Bump
 tau= 1;    %Constante para el termino feed-forward
 R = 1.5; % Rango de detección del sensor
-r = 1; % Distancia mínima aceptable a los obstáculos [x y]
+r = .5; % Distancia mínima aceptable a los obstáculos [x y] Debería ser <l
 agentes=4; %Número de agentes
 
 % Laplaciano grafo no dirigido
@@ -32,13 +32,13 @@ kronp=kron(Ld,I2);
 krontheta=kron(Ld,I1);
 
 % Inicializar condiciones iniciales para 4 robots
-z = [2;1;1.95; 0.95;3; -1; 2; -2]; % Cada fila es [x, y] de un robot
-thetaInicial = deg2rad([90, 0, -45, -90]); % Ángulos iniciales en radianes
-
+p_Inicial=[2;1;1.95; 0.95;3; -1; 2; -2]; %Tendrían que ser aleatorias, pero poner dos juntas pa que choquen (virtuales)
+thetaInicial = deg2rad([270, 360, 90, 180]); % Ángulos iniciales en radianes
+z = l*[cos(thetaInicial(1)); sin(thetaInicial(1)); cos(thetaInicial(2)); sin(thetaInicial(2)); cos(thetaInicial(3)); sin(thetaInicial(3)); cos(thetaInicial(4)); sin(thetaInicial(4))]; % Desplazamientos que me dan la formación en rombo
 
 % Datos de la simulación
 Dt = 0.01; % Periodo de muestreo
-tiempo = 30; % Duración de la simulación en segundos
+tiempo = 100; % Duración de la simulación en segundos
 iteraciones = tiempo / Dt;
 
 %inicializo v para poder calcular v_d_i
@@ -46,21 +46,25 @@ v_star=zeros(agentes,iteraciones);
 w_star=zeros(agentes,iteraciones);
 
 %Incicializo todos los valores
-p=zeros(8,iteraciones+1);
-p(:,1)=z;
-q_star=zeros(12,iteraciones);
-q_star(:,1)=[z(1:2);thetaInicial(1);z(3:4);thetaInicial(2);z(5:6);thetaInicial(3);z(7:8);thetaInicial(4);];
+p_star=zeros(8,iteraciones+1); %Agentes reales p_star
+p_star(:,1)=p_Inicial+z;
+p=zeros(8,iteraciones+1);      %Agentes virtuales p
+p(:,1)=p_Inicial; 
+theta=zeros(4,iteraciones+1);
+theta(:,1)=thetaInicial;
+theta_star=zeros(4,iteraciones);
+theta_star(:,1)=thetaInicial;
+theta_io=zeros(4,iteraciones);
+theta_ic=zeros(4,iteraciones);
+theta_bario=zeros(4,iteraciones);
+q_star=zeros(12,iteraciones);  %Definido como q_stari=[p_stari theta_star]
+q_star(:,1)=[p_star(1:2,1);theta_star(1,1);p_star(3:4,1);theta_star(2,1);p_star(5:6,1);theta_star(3,1);p(7:8,1);theta_star(4,1)];
 q_id=zeros(12,iteraciones);
 q_ic=zeros(12,iteraciones);
 q_iu=zeros(12,iteraciones);
 p_io=zeros(8,iteraciones);
 p_ia=p_io;
 p_ic=p_io;
-theta=zeros(4,iteraciones+1);
-theta(:,1)=thetaInicial;
-theta_io=zeros(4,iteraciones);
-theta_ic=zeros(4,iteraciones);
-theta_bario=zeros(4,iteraciones);
 e=zeros(12,iteraciones);
 %HAGO USO DE u_i_star antes de calcularlo entonces lo inicializo en zeros
 u_i_star=zeros(8,iteraciones+1);
@@ -68,11 +72,11 @@ t0_establecido=false;
 % Simulación
 for k = 1:iteraciones
         
-      %Aproximación de Euler de p estrella
-      p(:,k+1)=p(:,k)+Dt*(-kronp*p(:,k));
+%       %Aproximación de Euler de p estrella
+%       p_star(:,k+1)=p_star(:,k)+Dt*(-kronp*p_star(:,k));
         
-      %Aproximacion de Euler de theta estrella
-      theta(:,k+1)=theta(:,k)+Dt*(-krontheta*theta(:,k));
+%       %Aproximacion de Euler de theta estrella
+%       theta(:,k+1)=theta(:,k)+Dt*(-krontheta*theta(:,k));
 
      sump=zeros(8,1);
      % Bucle para calcular la suma de p
@@ -92,7 +96,7 @@ for k = 1:iteraciones
                         idx_i = 7:8; % Índices para el agente 4 (sump4_x, sump4_y)
                 end
                 % Sumar las coordenadas x e y del agente j a las correspondientes del agente i
-                sump(idx_i) = sump(idx_i) + p(idx_i,k+1);
+                sump(idx_i) = sump(idx_i) + p_star(idx_i,k);
             end
         end
      end
@@ -102,7 +106,7 @@ for k = 1:iteraciones
             for j = 1:agentes
                 %Verifico si existe una conexion entre agentes usando L
                 if L(i, j) == -1
-                    sumtheta(i) = sumtheta(i)+theta(j,k+1);
+                    sumtheta(i) = sumtheta(i)+theta_star(j,k);
                 end
             end
       end
@@ -114,7 +118,7 @@ for k = 1:iteraciones
     
 
       %Para calcular el vector de avoidance checo la distancia entre
-      %agentes uno por uno
+      %agentes virtuales uno por uno
       for i= 1:agentes
           switch i
                     case 1
@@ -130,16 +134,16 @@ for k = 1:iteraciones
               if i~= j
                 switch j
                     case 1
-                        id_j = 1:2; % Índices para el agente 1 
+                        id_j = 1:2; % Indices para el agente 1 
                     case 2
-                        id_j = 3:4; % Índices para el agente 2 
+                        id_j = 3:4; % Indices para el agente 2 
                     case 3
-                        id_j = 5:6; % Índices para el agente 3 
+                        id_j = 5:6; % Indices para el agente 3 
                     case 4
-                        id_j = 7:8; % Índices para el agente 4 
+                        id_j = 7:8; % Indices para el agente 4 
                 end
-                p_i=p(id_i,k);
-                p_o=p(id_j,k);
+                p_i=p_star(id_i,k)-z(id_i);
+                p_o=p_star(id_j,k)-z(id_j);
                 if norm(p_i-p_o)<r
                     p_io(id_i,k)=p_i-p_o;
                 end
@@ -166,7 +170,8 @@ for k = 1:iteraciones
       theta_bario(:,k)=theta(:,k+1)-theta_io(:,k);
 
       % Calculo vector atractor
-      % Bucle para calcular la suma de p
+      % Bucle para calcular la suma de p (Este es con los agentes reales)
+      sump=zeros(8,1);
       for i = 1:agentes
         for j = 1:agentes
             % Verificar si existe una conexión entre agentes usando L
@@ -183,11 +188,11 @@ for k = 1:iteraciones
                         idx_i = 7:8; % Índices para el agente 4 (sump4_x, sump4_y)
                 end
                 % Sumar las coordenadas x e y del agente j a las correspondientes del agente i
-                sump(idx_i) = sump(idx_i) + p(idx_i,k+1);
+                sump(idx_i) = sump(idx_i) + p_star(idx_i,k);
             end
         end
      end
-      p_ia(:,k)=(Di\sump*inv(Di))-p(:,k+1);
+      p_ia(:,k)=(Di\sump*inv(Di))-p_star(:,k);
       
       %Calculo el final del vector de avoidance que corresponde a p_ic
       for i=1:agentes
@@ -216,7 +221,7 @@ for k = 1:iteraciones
             case 4
                 id = [7 8]; % Índices para el agente 4 (sump4_x, sump4_y)
            end
-        theta_ic(i,k)=atan2(p_ic(id(2),k)-p(2,k+1),p_ic(id(1),k)-p(1,k+1));
+        theta_ic(i,k)=atan2(p_ic(id(2),k)-p_star(2,k),p_ic(id(1),k)-p_star(1,k));
       end
       
       %Calculo q_ic y lo acomodo de modo [x_ic,y_ic,theta_ic]
@@ -312,6 +317,9 @@ for k = 1:iteraciones
 
       %Calculo y acomodo u_i_star para graficarlos despues
       u_i_star(:,k+1)=[v_star(1,k) w_star(1,k) v_star(2,k) w_star(2,k) v_star(3,k) w_star(3,k) v_star(4,k) w_star(4,k)]';
+
+      p_star(:,k+1)=[q_star(1:2)'; q_star(4:5)';q_star(7:8)';q_star(10:11)'];
+      theta_star(:,k+1)=[q_star(3); q_star(6);q_star(9);q_star(12)'];
 end
 
 t = linspace(0, tiempo, iteraciones+1);
@@ -331,16 +339,27 @@ quiver(q_star(7, end), q_star(8, end), cos(q_star(9,end)), sin(q_star(9,end)), 0
 plot(q_star(10,:),q_star(11,:))
 quiver(q_star(10, 1), q_star(11, 1), cos(q_star(12,1)), sin(q_star(12,1)), 0.1, 'r', 'LineWidth', .1, 'MaxHeadSize', 1 );
 quiver(q_star(10, end), q_star(11, end), cos(q_star(12,end)), sin(q_star(12,end)), 0.1, 'r', 'LineWidth', .1, 'MaxHeadSize', 2);
-% plot(p(1,:),p(2,:))
-% plot(p(3,:),p(4,:))
-% plot(p(5,:),p(6,:))
-% plot(p(7,:),p(8,:))
 xlabel('x');
 ylabel('y');
 title('Trayectoria del robot no holónomo');
 hold off
 grid on
 
+figure
+hold on
+plot(p_star(1,:),p_star(2,:))
+plot(p_star(3,:),p_star(4,:))
+plot(p_star(5,:),p_star(6,:))
+plot(p_star(7,:),p_star(8,:))
+hold off
+
+figure
+hold on
+plot(p(1,:),p(2,:))
+plot(p(3,:),p(4,:))
+plot(p(5,:),p(6,:))
+plot(p(7,:),p(8,:))
+hold off
 
 t = linspace(0, tiempo, iteraciones);
 figure 
